6cc43d7a84d4b93ce84e9d0b979ca4e42af609df
ishikawayuto <yu.ishikawa.sys22@morijyobi.ac.jp>
2022-11-08 14:58:43 +0900

    2人分に修正

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
-
-public class Main{
+public class Main {
+	
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-                System.out.print("身長:");
+	 Scanner scan = new Scanner(System.in);
+	 for(int i=0; i<2;i++){
+                System.out.println("身長");
                 double height = scan.nextDouble();
-                System.out.print("体重:");
+                System.out.println("体重");
                 double weight = scan.nextDouble();
 		double height2 = height / 100.0;
-                double BMI = weight / Math.pow(height2,2);
+                double BMI = weight / Math.pow(height,2);
                 System.out.println((double)Math.round(BMI * 100)/100);
+	 }
                 scan.close();
 	}
 }
